bd6d5776afb3bcb59a7c87e6a06b09882d591124
MariaPaz04 <mpazmarcato@gmail.com>
2024-02-21 11:43:08 -0300

    Challenge Section 6

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int favoriteNumber;
+
+  cout << "Enter your favorite number between 1 and 100: ";
+  
+  cin >> favoriteNumber;
+
+  cout << "Amazing!" << favoriteNumber << "that's my favorite number too!" << endl;
+
+  return 0;
+}
