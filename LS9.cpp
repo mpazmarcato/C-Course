a2e8f07d88e3d773d3141b83f0af3bb89c87f566
Maria Paz <mpazmarcato@gmail.com>
2024-03-25 11:19:37 -0300

    While Loop

--- LS9.cpp
+++ LS9.cpp
@@ -347,3 +347,51 @@
         
 //     return 0;
 // }
+
+// While Loop
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+    
+//     // int num {};
+//     // cout << "Enter a positive integer - start the countdown: ";
+//     // cin >> num;
+
+//     // while (num > 0)
+//     // {
+//     //     cout << num << endl;
+//     //     --num;
+//     // }
+//     // cout << "Blastoff" << endl;
+
+//     // int num {};
+//     // cout << "Enter a positive integer - start the count up: ";
+//     // cin >> num;
+
+//     // int i {1};
+//     // while (num >= i)
+//     // {
+//     //     cout << i << endl;
+//     //     i++;
+//     // }
+//     // cout << "Blastoff" << endl;
+
+//     // bool done {false};
+//     // int number {0};
+
+//     // while (!done) {
+//     //         cout << "Enter an integer between 1 and 5: ";
+//     //         cin >> number;
+//     //         if (number <= 1 || number >= 5)
+//     //             cout << "Out of range, try again" << endl;
+//     //         else {
+//     //             cout << "Thanks!" << endl;
+//     //             done = true;
+//     //         }
+//     //     }
+
+//     cout  << endl;
+//     return 0;
+// }
